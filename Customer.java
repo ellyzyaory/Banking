76dc50b4968efab411ed09258c43f04622ec6080
ellyzyaory <71363310+ellyzyaory@users.noreply.github.com>
2021-04-13 21:57:13 +0700

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,29 @@
+package com.mycompany.bank;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private Account account;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account acct) {
+        account = acct;
+    }
+
+}
